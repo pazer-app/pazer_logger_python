113dc19420db70dc4d03c628782da8e430359f1d
davidkim <woody@woody.co.kr>
2024-12-19 19:44:32 +0900

    Version Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pazer_logger_python",
-    version="0.3.1",
+    version="0.4.1",
     packages=find_packages(),
     install_requires=[],
     description="Pazer Logger - Python",
